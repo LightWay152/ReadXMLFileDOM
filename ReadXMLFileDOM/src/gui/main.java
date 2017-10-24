4d1db5c057b590ad90f122ded5acdd90c63e900d
DESKTOP-UBC0EDN\DaoNguyen <dao.devlogs@gmail.com>
2017-10-25 00:32:52 +0700

    Read XML File by DOM

---
+++ ReadXMLFileDOM/src/gui/main.java
@@ -0,0 +1,13 @@
+package gui;
+
+/**
+ * @author DaoNguyen
+ */
+public class main{
+
+    public static void main(String[] args) {
+        //Using DOM
+        customerDOM frm = new customerDOM();
+        frm.setVisible(true);
+    }
+}
